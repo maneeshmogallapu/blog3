6d773a224daee500c0b675ca474682291655ca79
maneeshmogallapu <maneeshmogallapu@gmail.com>
2020-04-16 00:18:53 +0530

    first commit

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -2,11 +2,10 @@ function onClick() {
 	var i = document.getElementById('likes_id').value
 	$.ajax({
 		url: "/blog/ajax/likes",
-		data: {'i':i},
-		dataType : 'json',
+		data:{'i':i},
+		dataType: 'json',
 		success: function(data){
-			document.getElementById('like').innerHTML = data.i,likes
+			document.getElementById('like').innerHTML = data.i
 		}
 	})
 }
-
